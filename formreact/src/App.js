f2cf0169181568b6f17b212049b4d4e38041cb0d
25092003Hung <hungprodns1234@gmail.com>
2022-12-06 20:44:13 +0700

    first commit

---
+++ formreact/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ReactForm from './Components/ReactForm'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ReactForm />
+      </div>
+    )
+  }
+}
